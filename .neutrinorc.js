551b91783144d6bdfdb21e2192eafb1eedad6e16
Valentin Vago <valentin.vago@gmail.com>
2017-11-20 10:09:51 +0100

    add base scaffolding

---
+++ .neutrinorc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  use: [
+    ['neutrino-preset-airbnb-base'],
+    ['neutrino-middleware-styles-loader'],
+    ['neutrino-preset-web', {
+      html: {
+        title: 'A shop'
+      }
+    }],
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.scss';
+
+(($) => {
+  console.info('Hello world', $);
+})(window.jQuery);
