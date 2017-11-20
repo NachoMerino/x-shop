ff75082a16154b96f61382ed31bcdab40401750e
Valentin Vago <valentin.vago@gmail.com>
2017-11-20 11:35:24 +0100

    update neutrino config

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,10 +1,28 @@
+const path = require('path');
+
 module.exports = {
   use: [
     ['neutrino-preset-airbnb-base'],
-    ['neutrino-middleware-styles-loader'],
+    ['neutrino-middleware-styles-loader',{
+      minimize: true,
+      extractCSS: false,
+      sourceMap: true,
+      postcss: {},
+      cssModules: false,
+      autoprefixer: false,
+      externalLoaderOptions: {}
+    }],
     ['neutrino-preset-web', {
       html: {
-        title: 'A shop'
+        title: 'A shop',
+        mobile: true,
+        lang: 'en',
+        template: path.join(__dirname, 'src/index.ejs'),
+        // favicon: path.join(__dirname, 'src/static/favicon.png'),
+        minify: {
+          removeScriptTypeAttributes: true,
+          removeComments: true,
+        },
       }
     }],
   ],
