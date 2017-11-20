32cc432ef040cb171508340444c193a600122cda
Valentin Vago <valentin.vago@gmail.com>
2017-11-20 11:50:07 +0100

    add multiple pages

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const join = require('path').join;
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   use: [
@@ -17,13 +18,41 @@ module.exports = {
         title: 'A shop',
         mobile: true,
         lang: 'en',
-        template: path.join(__dirname, 'src/index.ejs'),
-        // favicon: path.join(__dirname, 'src/static/favicon.png'),
+        // favicon: join(__dirname, 'src/static/favicon.png'),
         minify: {
           removeScriptTypeAttributes: true,
           removeComments: true,
         },
       }
     }],
+    (neutrino) => {
+      // // Remove the default entry point and html plugin
+      // neutrino.config.entry('index').delete(neutrino.options.entry);
+      neutrino.config.plugins.delete('html');
+
+      const entries = ['index', 'details'];
+
+      entries.forEach(entry => neutrino.config
+        // .entry(entry)
+        //   .add(join(neutrino.options.source, `entry-${entry}.js`))
+        //   .end()
+        .plugin(`html-${entry}`)
+          .use(HtmlPlugin, [{
+              title: `A Shop | ${entry}`,
+              filename: `${entry}.html`,
+              template: join(neutrino.options.source, `html/${entry}.ejs`),
+              // The vendor/runtime chunk names here must match those in neutrino-middleware-chunk.
+              // chunks: [entry, 'vendor', 'runtime'],
+              chunks: ['index', 'vendor', 'runtime'],
+              // Copied from neutrino-middleware-html-template
+              minify: {
+                useShortDoctype: true,
+                keepClosingSlash: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: true
+              },
+          }])
+      );
+    },
   ],
 };
